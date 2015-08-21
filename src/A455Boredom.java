ad40848ef486884a4a29a33605e1618e39a569fa
olive <adalxh@gmail.com>
2015-08-21 21:28:32 +0800

    dp,has problem

---
+++ src/A455Boredom.java
@@ -0,0 +1,79 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Alex doesn't like boredom. That's why whenever he gets bored, he comes up
+ * with games. One long winter evening he came up with a game and decided to
+ * play it.
+ * 
+ * Given a sequence a consisting of n integers. The player can make several
+ * steps. In a single step he can choose an element of the sequence (let's
+ * denote it ak) and delete it, at that all elements equal to ak + 1 and ak - 1
+ * also must be deleted from the sequence. That step brings ak points to the
+ * player.
+ * 
+ * Alex is a perfectionist, so he decided to get as many points as possible.
+ * Help him.
+ * 
+ * Input 
+ * The first line contains integer n (1 ≤ n ≤ 105) that shows how many
+ * numbers are in Alex's sequence.
+ * 
+ * The second line contains n integers a1, a2, ..., an (1 ≤ ai ≤ 105).
+ * 
+ * Output 
+ * Print a single integer — the maximum number of points that Alex can
+ * earn.
+ * 
+ * Sample test(s) 
+ * input 
+ * 2 
+ * 1 2 
+ * output 
+ * 2 
+ * input 
+ * 3 
+ * 1 2 3 
+ * output 
+ * 4 
+ * input 
+ * 9 
+ * 1 2 1 3 2 2 2 2 3 
+ * output 
+ * 10 
+ * Note 
+ * Consider the third test example. At first step we need
+ * to choose any element equal to 2. After that step our sequence looks like
+ * this [2, 2, 2, 2]. Then we do 4 steps, on each step we choose any element
+ * equals to 2. In total we earn 10 points.
+ * 
+ * @Title: A455Boredom.java
+ * @author lxh
+ * @date Aug 21, 2015 8:53:21 PM
+ * 
+ */
+
+public class A455Boredom {
+	public static BigInteger max(BigInteger a,BigInteger b){
+		return a.compareTo(b)==-1?b:a;
+	}
+	public static void main(String []args){
+		Scanner cin=new Scanner(System.in);
+		int n=cin.nextInt();
+		int N=100005;
+		int []cnt=new int[N];
+		BigInteger a,b;
+		for(int i=0;i<n;i++){
+			int val=cin.nextInt();
+			cnt[val]++;
+		}
+		a=new BigInteger("0");
+		b=new BigInteger(String.valueOf(cnt[1]));
+		for(int i=2;i<N;i++){
+			BigInteger tmp=max(b,a.add(new BigInteger(String.valueOf(cnt[i]*i))));
+			a=b;
+			b=tmp;
+		}
+		System.out.println(b.toString());
+	}
+}

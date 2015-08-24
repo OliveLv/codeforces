8c019af26fe9f36a965cfea43956b134e0ddbdb4
olive <adalxh@gmail.com>
2015-08-24 19:27:01 +0800

    implementation

--- src/B266QueueAtTheSchool.java
+++ src/B266QueueAtTheSchool.java
@@ -10,15 +10,15 @@ import java.util.Scanner;
  * Let's describe the process more precisely. Let's say that the positions in
  * the queue are sequentially numbered by integers from 1 to n, at that the
  * person in the position number 1 is served first. Then, if at time x a boy
- * stands on the i-th position and a girl stands on the (i + 1)-th position,
- * then at time x + 1 the i-th position will have a girl and the (i + 1)-th
+ * stands on the i-th position and a girl stands on the (i鈥�鈥�)-th position,
+ * then at time x鈥�鈥� the i-th position will have a girl and the (i鈥�鈥�)-th
  * position will have a boy. The time is given in seconds.
  * 
  * You've got the initial position of the children, at the initial moment of
  * time. Determine the way the queue is going to look after t seconds.
  * 
  * Input 
- * The first line contains two integers n and t (1 ≤ n, t ≤ 50), which
+ * The first line contains two integers n and t (1鈥夆墹鈥塶,鈥塼鈥夆墹鈥�0), which
  * represent the number of children in the queue and the time after which the
  * queue will transform into the arrangement you need to find.
  * 
@@ -49,7 +49,7 @@ import java.util.Scanner;
  * output 
  * GGGB
  * 
- * @Title: B266QueueAtTheSchool.java
+ * @Title: A266QueueAtTheSchool.java
  * @author lxh
  * @date Aug 18, 2015 9:42:45 AM
  * 

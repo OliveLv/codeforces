41d2e58046c6bfcffdf6f72842df5fa106fbd951
olive <adalxh@gmail.com>
2015-09-12 13:54:49 +0800

    string

---
+++ src/A59Word.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * Vasya is very upset that many people on the Net mix uppercase and lowercase
+ * letters in one word. That's why he decided to invent an extension for his
+ * favorite browser that would change the letters' register in every word so
+ * that it either only consisted of lowercase letters or, vice versa, only of
+ * uppercase ones. At that as little as possible letters should be changed in
+ * the word. For example, the word HoUse must be replaced with house, and the
+ * word ViP ¡ª with VIP. If a word contains an equal number of uppercase and
+ * lowercase letters, you should replace all the letters with lowercase ones.
+ * For example, maTRIx should be replaced by matrix. Your task is to use the
+ * given method on one given word.
+ * 
+ * Input 
+ * The first line contains a word s ¡ª it consists of uppercase and
+ * lowercase Latin letters and possesses the length from 1 to 100.
+ * 
+ * Output 
+ * Print the corrected word s. If the given word s has strictly more
+ * uppercase letters, make the word written in the uppercase register, otherwise
+ * - in the lowercase one.
+ * 
+ * Sample test(s) 
+ * input 
+ * HoUse 
+ * output 
+ * house 
+ * input 
+ * ViP 
+ * output 
+ * VIP 
+ * input 
+ * maTRIx
+ * output 
+ * matrix
+ * 
+ * @Title: A59Word.java
+ * @author lxh
+ * @date Sep 12, 2015 1:42:31 PM
+ * 
+ */
+
+public class A59Word {
+	public static void main(String []args){
+		Scanner cin=new Scanner(System.in);	
+		String s=cin.next();
+		int un=0,ln=0;
+		for(int i=0;i<s.length();i++){
+			char val=s.charAt(i);
+			if(val>='a'&&val<='z')ln++;
+			else
+				un++;
+		}
+		if(ln>=un)System.out.println(s.toLowerCase());
+		else
+			System.out.println(s.toUpperCase());
+	}
+}
